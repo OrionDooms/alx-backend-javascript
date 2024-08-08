3cc4c2ff0b235324c59ec3d730076586ae4fafa6
Orion <Orion45.dd@gmail.com>
2024-08-08 18:39:22 +0000

    Import the ClassRoom, used initializeRooms function and return an array of 3 ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom';
+/* Implement a function initializeRooms.
+ * It should return an array of 3 ClassRoom objects. */
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34),
+  ];
+}
