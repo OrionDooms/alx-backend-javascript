8357020760ae47193075cf5c94787f3716fcbf3e
Orion <orion45.dd@gmail.com>
2024-09-30 11:50:53 -0700

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,11 @@
  * The user should be able to input their name on a new line.
  */
 console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
 	const Name = input.trim();
-	console.log(`Your name is: ${Name}`);
-	process.stdin.end();
+	process.stdout.write(`Your name is: ${Name}\n`);
 });
 
 process.stdin.on('end', function() {
