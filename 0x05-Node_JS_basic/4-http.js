769957f55fe8fb17939c90462884175d7c63eae5
Orion <orion45.dd@gmail.com>
2024-10-01 09:53:02 -0700

    curl localhost:1245/any_endpoint && echo

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,8 +9,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton school!');
 });
 
-app.listen(1245, () => {
-  console.log('...');
-});
+app.listen(1245);
 
 module.exports = app;
