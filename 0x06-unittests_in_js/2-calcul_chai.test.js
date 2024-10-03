0c32383aa014215711f17070cd547553ff600f10
Orion <orion45.dd@gmail.com>
2024-10-03 03:56:04 -0700

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,29 +1,37 @@
 const calculateNumber = require('./2-calcul_chai');
-const assert = require('chai');
+const { expect } = require('chai');
 
 describe('calculateNumber', function() {
-  it('SUM', function() {
-    assert.strictEqual(calculateNumber('SUM', 1.7, 3.3), 5);
+  describe('DIVIDE type', function(){
+    it('DIVIDE', function() {
+      expect(calculateNumber('DIVIDE', 15, 5)).to.equal(3);
+    });
+    it('DIVIDE', function() {
+      expect(calculateNumber('DIVIDE', 10, 5)).to.equal(2);
+    });
+    it('DIVIDE', function() {
+      expect(calculateNumber('DIVIDE', 10, 5)).to.equal(2);
+    });
+    it('DIVIDE', function() {
+      expect(calculateNumber('DIVIDE', 7.5, 0)).to.equal('Error');
+    });
   });
-  it('DIVIDE', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 15, 5), 3);
+
+  describe('SUM type', function(){
+    it('SUM', function() {
+      expect(calculateNumber('SUM', 1.7, 3.3)).to.equal(5);
+    });
+    it('SUM', function() {
+      expect(calculateNumber('SUM', 3.5, 3.5)).to.equal(8);
+    });
   });
-  it('SUBTRACT', function() {
-    assert.strictEqual(calculateNumber('SUBTRACT', 13.8, 6.8), 7);
-  });
-  it('DIVIDE', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
-  });
-  it('SUM', function() {
-    assert.strictEqual(calculateNumber('SUM', 3.5, 3.5), 8);
-  });
-  it('DIVIDE', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 10, 5), 2);
-  });
-  it('SUBTRACT', function() {
-    assert.strictEqual(calculateNumber('SUBTRACT', 9, 6), 3);
-  });
-  it('DIVIDE', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 7.5, 0), 'Error');
+
+  describe('SUBTRACT type', function(){
+    it('SUBTRACT', function() {
+      expect(calculateNumber('SUBTRACT', 13.8, 6.8)).to.equal(7);
+    });
+    it('SUBTRACT', function() {
+      expect(calculateNumber('SUBTRACT', 9, 6)).to.equal(3);
+    });
   });
 });
